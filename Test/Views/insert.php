cbde557341078bc08a2f8d90e029086b0b321eb3
garjona <gaag14@gmail.com>
2016-09-11 11:51:49 -0400

    Arreglado registrarse

--- Test/Views/insert.php
+++
@@ -1,9 +0,0 @@
-<?php
-$data = json_decode(file_get_contents("php://input"));
-$Nombre = mysql_real_escape_string($data->Nombre);
-$Mailj = mysql_real_escape_string($data->Mailj);
-$Contrasenia = mysql_real_escape_string($data->Contrasenia);
-mysql_connect("localhost","root","");
-mysql_select_db("scrapbook");
-mysql_query("INSERT INTO alumno (Mail, Nombre, Contrasenia, Tipo) VALUES('".$Mailj."','".$Nombre."','".$Contrasenia."','1')");
-?>
--- Test/server.js
+++ Test/server.js
@@ -14,7 +14,10 @@ var app = express();
 //ruteo
 
 app.use(express.static(__dirname + '/Views'));
+//Store all HTML files in view folder.
 app.use(express.static(__dirname + '/'));
+//Store all JS and CSS in Scripts folder.
+
 app.get('/', function(req,res) {
 	res.sendfile(__dirname + '/Views/index.html');
 });
@@ -92,15 +95,33 @@ function recuperar(pedido,respuesta) {
     pedido.on('end', function(){
         var formulario = querystring.parse(info);
 		respuesta.writeHead(200, {'Content-Type': 'text/html'});
-		var pagina='<!doctype html><html><head><title>FisFinder120 Home</title><meta charset="utf-8" />' +
-			'<meta name="viewport" content="width=device-width, initial-scale=1" />' +
-			'<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->' +
-			'<link rel="stylesheet" href="assets/css/main.css" />' +
-			'<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]--></head><body>'+
-		           'Nombre de usuario:'+formulario['nombre']+'<br>'+
-				   'Clave:'+formulario['clave']+'<br>'+
-				   '<a href="index.html">Retornar</a>'+
-		           '</body></html>';
+		var pagina='<!doctype html><html><head><title>Registro exitoso</title><meta charset="utf-8" />' +
+        '<meta http-equiv="content-type" content="text/html; charset=utf-8" /><meta name="description" content="" /><meta name="keywords" content="" />' +
+        "<link href='http://fonts.googleapis.com/css?family=Raleway:400,100,200,300,500,600,700,800,900' rel='stylesheet' type='text/css'>"+
+        '<script src="js/html5shiv.js"></script><![endif]-->'+
+        '<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>'+
+        '<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.7/angular.min.js"></script>'+
+        '<script src="js/skel.min.js"></script>'+
+        '<script src="js/skel-panels.min.js"></script>'+
+        '<script src="js/init.js"></script>'+
+        '<script src="server.js"></script>"'+
+        '<noscript> <link rel="stylesheet" href="../css/skel-noscript.css" /> <link rel="stylesheet" href="../css/style.css" /> <link rel="stylesheet" href="../css/style-desktop.css" /> </noscript>'+
+        ' <link rel="stylesheet" href="../css/ie/v9.css"/><![endif]-->' +
+        '</head><body>'+
+        '<div id="banner"></div>'+
+        '<div id="header"> <div class="container"> <!-- Logo --> <div id="logo"> <h1><a href="#">ScrapBooks</a></h1> </div> <!-- Nav --> <nav id="nav"> <ul> <li ><a href="index.html">Inicio</a></li> <li><a href="IniciarSesion.html">Iniciar Sesión</a></li> <li class="active"><a href="Registrarse.html">Registrarse</a></li> <li><a href="Test.html">Test</a></li> </ul> </nav> </div> </div>'+
+        '<div id="featured">'+
+        '<div class="container">'+
+        '<div class="row">'+
+        '<div class="12u">'+
+        '<section>'+
+        '<header>'+
+        '<h2>Registro exitoso</h2>'+
+        '</header>'+
+        'Nombre de usuario:'+formulario['nombre']+'<br>'+
+		'Clave:'+formulario['clave']+'<br>'+
+		'<a href="index.html">Retornar</a>'+
+		'</body></html>';
 		respuesta.end(pagina);
 		var datos = {mail: formulario['email'], nombre: formulario['nombre']  , contrasenia: formulario['clave'],  tipo: '0'};
 		connection.connect();
@@ -112,4 +133,4 @@ function recuperar(pedido,respuesta) {
     });	
 }
 
-console.log('Servidor web iniciado');
+console.log('Servidor web iniciado en http://localhost:9000');

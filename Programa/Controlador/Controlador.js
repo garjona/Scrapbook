052ae6d2f7db5f1ea8fcf68e0b63bdb6c253e03b
foltrex <j.alistea@gmail.com>
2016-10-24 02:20:47 -0300

    Add files via upload

--- Programa/Controlador/Controlador.js
+++ Programa/Controlador/Controlador.js
@@ -40,6 +40,33 @@ app.controller('Datos', function ($scope) {
 
 });
 
+app.controller('InlineEditorController', function($scope){
+
+    // $scope is a special object that makes
+    // its properties available to the view as
+    // variables. Here we set some default values:
+
+    $scope.showtooltip = false;
+    //$scope.value = document.getElementById('asd').textContent;
+
+    // Some helper functions that will be
+    // available in the angular declarations
+
+    $scope.hideTooltip = function(){
+
+        // When a model is changed, the view will be automatically
+        // updated by by AngularJS. In this case it will hide the tooltip.
+
+        $scope.showtooltip = false;
+    }
+
+    $scope.toggleTooltip = function(e){
+        e.stopPropagation();
+        $scope.showtooltip = !$scope.showtooltip;
+    }
+}
+);
+
 app.config(function($routeProvider) {
     $routeProvider
 
